74600144a314dba31e64bbc0eec00564aafb1946
freshie-Dev <hazardpubg1122@gmail.com>
2023-09-18 21:51:03 +0500

    3rd commit

--- index.cjs
+++ index.cjs
@@ -8,7 +8,9 @@ app.use(cors());
 
 const hostname = '0.0.0.0';
 
-// app.use('/articles', articleRoutes);
+app.get('/', (req,res) => {
+  res.send("Hello World!")
+})
 app.use('/products', require('./routes/productRoutes.cjs'));
 app.use('/register', require('./routes/userRoutes.cjs'));
 
@@ -17,6 +19,10 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, function() {
-    console.log('Server started on port 3000');
-    });
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server started on port ' + (process.env.PORT || 3000));
+});
+// app.listen(port, function() {
+//     console.log('Server started on port 3000');
+//     });
