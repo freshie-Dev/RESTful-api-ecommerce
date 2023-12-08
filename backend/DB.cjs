4db522a9f3a21420d29e930286e03b264eb553d1
freshie-Dev <hazardpubg1122@gmail.com>
2023-12-08 21:09:36 +0500

    changed DB

--- backend/DB.cjs
+++ backend/DB.cjs
@@ -10,7 +10,7 @@ const options = {
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://dbUser:admin123@cluster0.dk70mwt.mongodb.net/ShopEase', options);
+        await mongoose.connect('mongodb+srv://dbUser:admin123@cluster0.dk70mwt.mongodb.net/EcommerceDB', options);
         // await mongoose.connect('mongodb+srv://dbUser:admin@cluster0.dk70mwt.mongodb.net/EcommerceDB', options);
         console.log('Connected to MongoDB')
 
